cf86ac066e5256788b5e9668ca197b554ec75333
Maximo Del Valle <maximo5741@outlook.com>
2024-09-03 22:18:12 -0400

    Removed the unnecessary content and folders

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<?php include_once('./header.php');?>
+<div class="container">
+  <p>Filler content</p>
+</div>
+     
+<div class="container">
+  <p>Filler content</p>
+</div>
+
+<div class="container">
+  <!-- Content here -->
+   
+</div>
+
+<?php include_once('./footer.php'); ?>
